eaf4a83c51c4a4d04dbf94aa0180ffc9bd182eb7
jacquelinemarinho <63355502+jacquelinemarinho@users.noreply.github.com>
2021-10-27 23:52:31 -0300

    adicionando botao tela inteira

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main(int argc, char **argv) {
     abcg::Application app(argc, argv);
 
     auto window{std::make_unique<OpenGLWindow>()};
-    window->setOpenGLSettings({.samples = 4});
+    window->setOpenGLSettings({.profile = abcg::OpenGLProfile::Core,
+                               .majorVersion = 4,
+                               .minorVersion = 1,
+				.samples = 4});
     window->setWindowSettings({.width = 600,
                                .height = 600,
-                               .showFPS = false,
-                               .showFullscreenButton = false,
+                               .showFPS = false,                              
                                .title = "Asteroids"});
     app.run(std::move(window));
   } catch (const abcg::Exception &exception) {
