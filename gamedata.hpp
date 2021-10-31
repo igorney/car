2c97c6fba4c70d05f8f881f4244a2106893d7c0a
jacquelinemarinho <jacque_marinho@hotmail.com>
2021-10-30 21:45:34 -0300

    removendo atributos nao necessarios

--- gamedata.hpp
+++ gamedata.hpp
@@ -3,12 +3,12 @@
 
 #include <bitset>
 
-enum class Input { Right, Left, Down, Up, Stop };
+enum class Input { Right, Left, Up, Stop };
 enum class State { Playing, Win };
 
 struct GameData {
   State m_state{State::Playing};
-  std::bitset<5> m_input;  
+  std::bitset<4> m_input;  
 };
 
 #endif
--- openglwindow.cpp
+++ openglwindow.cpp
@@ -12,25 +12,21 @@
 int m_objects = 0;
 
 void OpenGLWindow::handleEvent(SDL_Event &event) {  
-  if (event.type == SDL_KEYDOWN) {
+  if (event.type == SDL_KEYDOWN || event.key.keysym.sym == SDLK_s) {
     if (event.key.keysym.sym == SDLK_SPACE)
       m_gameData.m_input.set(static_cast<size_t>(Input::Stop));
     if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w)
-      m_gameData.m_input.set(static_cast<size_t>(Input::Up));
-    if (event.key.keysym.sym == SDLK_DOWN || event.key.keysym.sym == SDLK_s)
-      m_gameData.m_input.set(static_cast<size_t>(Input::Down));
+      m_gameData.m_input.set(static_cast<size_t>(Input::Up));    
     if (event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_a)
       m_gameData.m_input.set(static_cast<size_t>(Input::Left));
     if (event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d)
       m_gameData.m_input.set(static_cast<size_t>(Input::Right));
   }
   if (event.type == SDL_KEYUP) {
-    if (event.key.keysym.sym == SDLK_SPACE)
+    if (event.key.keysym.sym == SDLK_SPACE || event.key.keysym.sym == SDLK_s)
       m_gameData.m_input.reset(static_cast<size_t>(Input::Stop));
     if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_w)
-      m_gameData.m_input.reset(static_cast<size_t>(Input::Up));
-    if (event.key.keysym.sym == SDLK_DOWN || event.key.keysym.sym == SDLK_s)
-      m_gameData.m_input.reset(static_cast<size_t>(Input::Down));
+      m_gameData.m_input.reset(static_cast<size_t>(Input::Up));    
     if (event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_a)
       m_gameData.m_input.reset(static_cast<size_t>(Input::Left));
     if (event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_d)
